ff3bc8fcf27d1caf972016d718b33099a762c14f
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-08 23:54:46 -0400

    Use entity class attributes for cpuspeed

--- homeassistant/components/cpuspeed/sensor.py
+++ homeassistant/components/cpuspeed/sensor.py
@@ -31,26 +31,13 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class CpuSpeedSensor(SensorEntity):
     """Representation of a CPU sensor."""
 
+    _attr_icon = ICON
+    _attr_unit_of_measurement = FREQUENCY_GIGAHERTZ
+
     def __init__(self, name):
         """Initialize the CPU sensor."""
-        self._name = name
-        self._state = None
+        self._attr_name = name
         self.info = None
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit the value is expressed in."""
-        return FREQUENCY_GIGAHERTZ
 
     @property
     def extra_state_attributes(self):
@@ -64,15 +51,10 @@ class CpuSpeedSensor(SensorEntity):
                 attrs[ATTR_HZ] = round(self.info[HZ_ADVERTISED][0] / 10 ** 9, 2)
             return attrs
 
-    @property
-    def icon(self):
-        """Return the icon to use in the frontend, if any."""
-        return ICON
-
     def update(self):
         """Get the latest data and updates the state."""
         self.info = cpuinfo.get_cpu_info()
         if HZ_ACTUAL in self.info:
-            self._state = round(float(self.info[HZ_ACTUAL][0]) / 10 ** 9, 2)
+            self._attr_state = round(float(self.info[HZ_ACTUAL][0]) / 10 ** 9, 2)
         else:
-            self._state = None
+            self._attr_state = None
