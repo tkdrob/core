364c6e085612b1187b7b315222cefec12a4e6b6d
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-11 18:54:30 -0400

    Use entity class attributes for etherscan

--- homeassistant/components/etherscan/sensor.py
+++ homeassistant/components/etherscan/sensor.py
@@ -46,39 +46,19 @@ class EtherscanSensor(SensorEntity):
 
     def __init__(self, name, address, token, token_address):
         """Initialize the sensor."""
-        self._name = name
+        self._attr_name = name
         self._address = address
         self._token_address = token_address
         self._token = token
-        self._state = None
-        self._unit_of_measurement = self._token or "ETH"
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement this sensor expresses itself in."""
-        return self._unit_of_measurement
-
-    @property
-    def extra_state_attributes(self):
-        """Return the state attributes of the sensor."""
-        return {ATTR_ATTRIBUTION: ATTRIBUTION}
+        self._attr_unit_of_measurement = self._token or "ETH"
 
     def update(self):
         """Get the latest state of the sensor."""
 
         if self._token_address:
-            self._state = get_balance(self._address, self._token_address)
+            self._attr_state = get_balance(self._address, self._token_address)
         elif self._token:
-            self._state = get_balance(self._address, self._token)
+            self._attr_state = get_balance(self._address, self._token)
         else:
-            self._state = get_balance(self._address)
+            self._attr_state = get_balance(self._address)
+        self._attr_extra_state_attributes = {ATTR_ATTRIBUTION: ATTRIBUTION}
