1b12d463194a03d7ed98f258f95d7a88b4f3a0ee
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-11 16:23:10 -0400

    Use entity class attributes for entur_public_trans

--- homeassistant/components/entur_public_transport/sensor.py
+++ homeassistant/components/entur_public_transport/sensor.py
@@ -150,6 +150,9 @@ class EnturProxy:
 class EnturPublicTransportSensor(SensorEntity):
     """Implementation of a Entur public transport sensor."""
 
+    _attr_icon = ICONS[DEFAULT_ICON_KEY]
+    _attr_unit_of_measurement = TIME_MINUTES
+
     def __init__(
         self, api: EnturProxy, name: str, stop: str, show_on_map: bool
     ) -> None:
@@ -157,90 +160,62 @@ class EnturPublicTransportSensor(SensorEntity):
         self.api = api
         self._stop = stop
         self._show_on_map = show_on_map
-        self._name = name
-        self._state = None
-        self._icon = ICONS[DEFAULT_ICON_KEY]
-        self._attributes = {}
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self) -> str:
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def extra_state_attributes(self) -> dict:
-        """Return the state attributes."""
-        self._attributes[ATTR_ATTRIBUTION] = ATTRIBUTION
-        self._attributes[ATTR_STOP_ID] = self._stop
-        return self._attributes
-
-    @property
-    def unit_of_measurement(self) -> str:
-        """Return the unit this state is expressed in."""
-        return TIME_MINUTES
-
-    @property
-    def icon(self) -> str:
-        """Icon to use in the frontend."""
-        return self._icon
+        self._attr_name = name
 
     async def async_update(self) -> None:
         """Get the latest data and update the states."""
         await self.api.async_update()
 
-        self._attributes = {}
+        self._attr_extra_state_attributes = {}
 
         data = self.api.get_stop_info(self._stop)
         if data is None:
-            self._state = None
+            self._attr_state = None
             return
 
         if self._show_on_map and data.latitude and data.longitude:
-            self._attributes[CONF_LATITUDE] = data.latitude
-            self._attributes[CONF_LONGITUDE] = data.longitude
+            self._attr_extra_state_attributes[CONF_LATITUDE] = data.latitude
+            self._attr_extra_state_attributes[CONF_LONGITUDE] = data.longitude
 
         calls = data.estimated_calls
         if not calls:
-            self._state = None
+            self._attr_state = None
             return
 
-        self._state = due_in_minutes(calls[0].expected_departure_time)
-        self._icon = ICONS.get(calls[0].transport_mode, ICONS[DEFAULT_ICON_KEY])
+        self._attr_state = due_in_minutes(calls[0].expected_departure_time)
+        self._attr_icon = ICONS.get(calls[0].transport_mode, ICONS[DEFAULT_ICON_KEY])
 
-        self._attributes[ATTR_ROUTE] = calls[0].front_display
-        self._attributes[ATTR_ROUTE_ID] = calls[0].line_id
-        self._attributes[ATTR_EXPECTED_AT] = calls[0].expected_departure_time.strftime(
-            "%H:%M"
-        )
-        self._attributes[ATTR_REALTIME] = calls[0].is_realtime
-        self._attributes[ATTR_DELAY] = calls[0].delay_in_min
+        self._attr_extra_state_attributes[ATTR_ROUTE] = calls[0].front_display
+        self._attr_extra_state_attributes[ATTR_ROUTE_ID] = calls[0].line_id
+        self._attr_extra_state_attributes[ATTR_EXPECTED_AT] = calls[
+            0
+        ].expected_departure_time.strftime("%H:%M")
+        self._attr_extra_state_attributes[ATTR_REALTIME] = calls[0].is_realtime
+        self._attr_extra_state_attributes[ATTR_DELAY] = calls[0].delay_in_min
 
         number_of_calls = len(calls)
         if number_of_calls < 2:
             return
 
-        self._attributes[ATTR_NEXT_UP_ROUTE] = calls[1].front_display
-        self._attributes[ATTR_NEXT_UP_ROUTE_ID] = calls[1].line_id
-        self._attributes[ATTR_NEXT_UP_AT] = calls[1].expected_departure_time.strftime(
-            "%H:%M"
-        )
-        self._attributes[
+        self._attr_extra_state_attributes[ATTR_NEXT_UP_ROUTE] = calls[1].front_display
+        self._attr_extra_state_attributes[ATTR_NEXT_UP_ROUTE_ID] = calls[1].line_id
+        self._attr_extra_state_attributes[ATTR_NEXT_UP_AT] = calls[
+            1
+        ].expected_departure_time.strftime("%H:%M")
+        self._attr_extra_state_attributes[
             ATTR_NEXT_UP_IN
         ] = f"{due_in_minutes(calls[1].expected_departure_time)} min"
-        self._attributes[ATTR_NEXT_UP_REALTIME] = calls[1].is_realtime
-        self._attributes[ATTR_NEXT_UP_DELAY] = calls[1].delay_in_min
+        self._attr_extra_state_attributes[ATTR_NEXT_UP_REALTIME] = calls[1].is_realtime
+        self._attr_extra_state_attributes[ATTR_NEXT_UP_DELAY] = calls[1].delay_in_min
 
         if number_of_calls < 3:
             return
 
         for i, call in enumerate(calls[2:]):
             key_name = f"departure_#{i + 3}"
-            self._attributes[key_name] = (
+            self._attr_extra_state_attributes[key_name] = (
                 f"{'' if bool(call.is_realtime) else 'ca. '}"
                 f"{call.expected_departure_time.strftime('%H:%M')} {call.front_display}"
             )
+        self._attr_extra_state_attributes[ATTR_ATTRIBUTION] = ATTRIBUTION
+        self._attr_extra_state_attributes[ATTR_STOP_ID] = self._stop
