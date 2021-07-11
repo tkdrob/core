d49fa9479ca559b833ad24bbf352d731cf1ddb91
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-11 11:05:10 -0400

    Use entity class attributes for ebox

--- homeassistant/components/ebox/sensor.py
+++ homeassistant/components/ebox/sensor.py
@@ -94,39 +94,17 @@ class EBoxSensor(SensorEntity):
 
     def __init__(self, ebox_data, sensor_type, name):
         """Initialize the sensor."""
-        self.client_name = name
         self.type = sensor_type
-        self._name = SENSOR_TYPES[sensor_type][0]
-        self._unit_of_measurement = SENSOR_TYPES[sensor_type][1]
-        self._icon = SENSOR_TYPES[sensor_type][2]
+        self._attr_name = f"{name} {SENSOR_TYPES[sensor_type][0]}"
+        self._attr_unit_of_measurement = SENSOR_TYPES[sensor_type][1]
+        self._attr_icon = SENSOR_TYPES[sensor_type][2]
         self.ebox_data = ebox_data
-        self._state = None
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return f"{self.client_name} {self._name}"
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        return self._icon
 
     async def async_update(self):
         """Get the latest data from EBox and update the state."""
         await self.ebox_data.async_update()
         if self.type in self.ebox_data.data:
-            self._state = round(self.ebox_data.data[self.type], 2)
+            self._attr_state = round(self.ebox_data.data[self.type], 2)
 
 
 class EBoxData:
