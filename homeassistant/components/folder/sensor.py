3caa4c8a198f9e3ae939402a9d30b9ca7d0e1d4b
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-13 15:41:42 -0400

    Use entity class attributes for folder

--- homeassistant/components/folder/sensor.py
+++ homeassistant/components/folder/sensor.py
@@ -53,55 +53,25 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class Folder(SensorEntity):
     """Representation of a folder."""
 
-    ICON = "mdi:folder"
+    _attr_icon = "mdi:folder"
+    _attr_unit_of_measurement = DATA_MEGABYTES
 
     def __init__(self, folder_path, filter_term):
         """Initialize the data object."""
         folder_path = os.path.join(folder_path, "")  # If no trailing / add it
         self._folder_path = folder_path  # Need to check its a valid path
         self._filter_term = filter_term
-        self._number_of_files = None
-        self._size = None
-        self._name = os.path.split(os.path.split(folder_path)[0])[1]
-        self._unit_of_measurement = DATA_MEGABYTES
-        self._file_list = None
+        self._attr_name = os.path.split(os.path.split(folder_path)[0])[1]
 
     def update(self):
         """Update the sensor."""
-        files_list = get_files_list(self._folder_path, self._filter_term)
-        self._file_list = files_list
-        self._number_of_files = len(files_list)
-        self._size = get_size(files_list)
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        decimals = 2
-        size_mb = round(self._size / 1e6, decimals)
-        return size_mb
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        return self.ICON
-
-    @property
-    def extra_state_attributes(self):
-        """Return other details about the sensor state."""
-        return {
+        file_list = get_files_list(self._folder_path, self._filter_term)
+        size = get_size(file_list)
+        self._attr_state = round(size / 1e6, 2)
+        self._attr_extra_state_attributes = {
             "path": self._folder_path,
             "filter": self._filter_term,
-            "number_of_files": self._number_of_files,
-            "bytes": self._size,
-            "file_list": self._file_list,
+            "number_of_files": len(file_list),
+            "bytes": size,
+            "file_list": file_list,
         }
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        return self._unit_of_measurement
