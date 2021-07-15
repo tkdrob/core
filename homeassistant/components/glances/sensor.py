cafe886e4726705bb48b4d41cc5791738391f44a
Robert Hillis <tkdrob4390@yahoo.com>
2021-07-15 09:47:29 -0400

    Use entity class attributes for glances

--- homeassistant/components/glances/sensor.py
+++ homeassistant/components/glances/sensor.py
@@ -62,6 +62,8 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 class GlancesSensor(SensorEntity):
     """Implementation of a Glances sensor."""
 
+    _attr_should_poll = False
+
     def __init__(
         self,
         glances_data,
@@ -74,52 +76,14 @@ class GlancesSensor(SensorEntity):
         """Initialize the sensor."""
         self.glances_data = glances_data
         self._sensor_name_prefix = sensor_name_prefix
-        self._sensor_name_suffix = sensor_name_suffix
-        self._name = name
+        self._attr_name = f"{name} {sensor_name_prefix} {sensor_name_suffix}"
+        self._attr_icon = sensor_details[3]
+        self._attr_unique_id = f"{glances_data.host}-{self.name}"
         self.type = sensor_type
-        self._state = None
         self.sensor_details = sensor_details
         self.unsub_update = None
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return f"{self._name} {self._sensor_name_prefix} {self._sensor_name_suffix}"
-
-    @property
-    def unique_id(self):
-        """Set unique_id for sensor."""
-        return f"{self.glances_data.host}-{self.name}"
-
-    @property
-    def device_class(self):
-        """Return the class of this device, from component DEVICE_CLASSES."""
-        return self.sensor_details[4]
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        return self.sensor_details[3]
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit the value is expressed in."""
-        return self.sensor_details[2]
-
-    @property
-    def available(self):
-        """Could the device be accessed during the last update call."""
-        return self.glances_data.available
-
-    @property
-    def state(self):
-        """Return the state of the resources."""
-        return self._state
-
-    @property
-    def should_poll(self):
-        """Return the polling requirement for this sensor."""
-        return False
+        self._attr_device_class = sensor_details[4]
+        self._attr_unit_of_measurement = sensor_details[2]
 
     async def async_added_to_hass(self):
         """Handle entity which will be added."""
@@ -139,6 +103,7 @@ class GlancesSensor(SensorEntity):
 
     async def async_update(self):  # noqa: C901
         """Get the latest data from REST API."""
+        self._attr_available = self.glances_data.available
         value = self.glances_data.api.data
         if value is None:
             return
@@ -150,96 +115,96 @@ class GlancesSensor(SensorEntity):
                     break
             if self.type == "disk_free":
                 try:
-                    self._state = round(disk["free"] / 1024 ** 3, 1)
+                    self._attr_state = round(disk["free"] / 1024 ** 3, 1)
                 except KeyError:
-                    self._state = round(
+                    self._attr_state = round(
                         (disk["size"] - disk["used"]) / 1024 ** 3,
                         1,
                     )
             elif self.type == "disk_use":
-                self._state = round(disk["used"] / 1024 ** 3, 1)
+                self._attr_state = round(disk["used"] / 1024 ** 3, 1)
             elif self.type == "disk_use_percent":
-                self._state = disk["percent"]
+                self._attr_state = disk["percent"]
         elif self.type == "battery":
             for sensor in value["sensors"]:
                 if (
                     sensor["type"] == "battery"
                     and sensor["label"] == self._sensor_name_prefix
                 ):
-                    self._state = sensor["value"]
+                    self._attr_state = sensor["value"]
         elif self.type == "fan_speed":
             for sensor in value["sensors"]:
                 if (
                     sensor["type"] == "fan_speed"
                     and sensor["label"] == self._sensor_name_prefix
                 ):
-                    self._state = sensor["value"]
+                    self._attr_state = sensor["value"]
         elif self.type == "temperature_core":
             for sensor in value["sensors"]:
                 if (
                     sensor["type"] == "temperature_core"
                     and sensor["label"] == self._sensor_name_prefix
                 ):
-                    self._state = sensor["value"]
+                    self._attr_state = sensor["value"]
         elif self.type == "temperature_hdd":
             for sensor in value["sensors"]:
                 if (
                     sensor["type"] == "temperature_hdd"
                     and sensor["label"] == self._sensor_name_prefix
                 ):
-                    self._state = sensor["value"]
+                    self._attr_state = sensor["value"]
         elif self.type == "memory_use_percent":
-            self._state = value["mem"]["percent"]
+            self._attr_state = value["mem"]["percent"]
         elif self.type == "memory_use":
-            self._state = round(value["mem"]["used"] / 1024 ** 2, 1)
+            self._attr_state = round(value["mem"]["used"] / 1024 ** 2, 1)
         elif self.type == "memory_free":
-            self._state = round(value["mem"]["free"] / 1024 ** 2, 1)
+            self._attr_state = round(value["mem"]["free"] / 1024 ** 2, 1)
         elif self.type == "swap_use_percent":
-            self._state = value["memswap"]["percent"]
+            self._attr_state = value["memswap"]["percent"]
         elif self.type == "swap_use":
-            self._state = round(value["memswap"]["used"] / 1024 ** 3, 1)
+            self._attr_state = round(value["memswap"]["used"] / 1024 ** 3, 1)
         elif self.type == "swap_free":
-            self._state = round(value["memswap"]["free"] / 1024 ** 3, 1)
+            self._attr_state = round(value["memswap"]["free"] / 1024 ** 3, 1)
         elif self.type == "processor_load":
             # Windows systems don't provide load details
             try:
-                self._state = value["load"]["min15"]
+                self._attr_state = value["load"]["min15"]
             except KeyError:
-                self._state = value["cpu"]["total"]
+                self._attr_state = value["cpu"]["total"]
         elif self.type == "process_running":
-            self._state = value["processcount"]["running"]
+            self._attr_state = value["processcount"]["running"]
         elif self.type == "process_total":
-            self._state = value["processcount"]["total"]
+            self._attr_state = value["processcount"]["total"]
         elif self.type == "process_thread":
-            self._state = value["processcount"]["thread"]
+            self._attr_state = value["processcount"]["thread"]
         elif self.type == "process_sleeping":
-            self._state = value["processcount"]["sleeping"]
+            self._attr_state = value["processcount"]["sleeping"]
         elif self.type == "cpu_use_percent":
-            self._state = value["quicklook"]["cpu"]
+            self._attr_state = value["quicklook"]["cpu"]
         elif self.type == "docker_active":
             count = 0
             try:
                 for container in value["docker"]["containers"]:
                     if container["Status"] == "running" or "Up" in container["Status"]:
                         count += 1
-                self._state = count
+                self._attr_state = count
             except KeyError:
-                self._state = count
+                self._attr_state = count
         elif self.type == "docker_cpu_use":
             cpu_use = 0.0
             try:
                 for container in value["docker"]["containers"]:
                     if container["Status"] == "running" or "Up" in container["Status"]:
                         cpu_use += container["cpu"]["total"]
-                    self._state = round(cpu_use, 1)
+                    self._attr_state = round(cpu_use, 1)
             except KeyError:
-                self._state = STATE_UNAVAILABLE
+                self._attr_state = STATE_UNAVAILABLE
         elif self.type == "docker_memory_use":
             mem_use = 0.0
             try:
                 for container in value["docker"]["containers"]:
                     if container["Status"] == "running" or "Up" in container["Status"]:
                         mem_use += container["memory"]["usage"]
-                    self._state = round(mem_use / 1024 ** 2, 1)
+                    self._attr_state = round(mem_use / 1024 ** 2, 1)
             except KeyError:
-                self._state = STATE_UNAVAILABLE
+                self._attr_state = STATE_UNAVAILABLE
