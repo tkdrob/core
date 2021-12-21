72b499047212e2827613e7e574e91a2133b6bf36
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-21 04:40:08 -0500

    Simplify hass data in airnow

--- homeassistant/components/airnow/__init__.py
+++ homeassistant/components/airnow/__init__.py
@@ -62,9 +62,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
 
     # Store Entity and Initialize Platforms
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
