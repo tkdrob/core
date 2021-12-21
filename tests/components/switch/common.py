f3d531740512d8454afdd16abe1567bad8fe0bb6
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-21 18:18:35 -0500

    Use platform enums in switch tests

--- tests/components/switch/common.py
+++ tests/components/switch/common.py
@@ -3,12 +3,12 @@
 All containing methods are legacy helpers that should not be used by new
 components. Instead call the service directly.
 """
-from homeassistant.components.switch import DOMAIN
 from homeassistant.const import (
     ATTR_ENTITY_ID,
     ENTITY_MATCH_ALL,
     SERVICE_TURN_OFF,
     SERVICE_TURN_ON,
+    Platform,
 )
 from homeassistant.loader import bind_hass
 
@@ -22,7 +22,9 @@ def turn_on(hass, entity_id=ENTITY_MATCH_ALL):
 async def async_turn_on(hass, entity_id=ENTITY_MATCH_ALL):
     """Turn all or specified switch on."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else None
-    await hass.services.async_call(DOMAIN, SERVICE_TURN_ON, data, blocking=True)
+    await hass.services.async_call(
+        Platform.SWITCH, SERVICE_TURN_ON, data, blocking=True
+    )
 
 
 @bind_hass
@@ -34,4 +36,6 @@ def turn_off(hass, entity_id=ENTITY_MATCH_ALL):
 async def async_turn_off(hass, entity_id=ENTITY_MATCH_ALL):
     """Turn all or specified switch off."""
     data = {ATTR_ENTITY_ID: entity_id} if entity_id else None
-    await hass.services.async_call(DOMAIN, SERVICE_TURN_OFF, data, blocking=True)
+    await hass.services.async_call(
+        Platform.SWITCH, SERVICE_TURN_OFF, data, blocking=True
+    )
