ae9dc3b5ac1de3826179e4961a14fe1b9e5bc732
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-21 05:13:09 -0500

    Simplify hass data in azure_devops

--- homeassistant/components/azure_devops/__init__.py
+++ homeassistant/components/azure_devops/__init__.py
@@ -78,9 +78,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator, project
-
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator, project
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
