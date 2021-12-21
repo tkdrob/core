67e31f6923cd0fa0c681d9e91c2b3f3aa90f6e58
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-21 05:11:49 -0500

    Smplify hass data in awair

--- homeassistant/components/awair/__init__.py
+++ homeassistant/components/awair/__init__.py
@@ -25,9 +25,7 @@ async def async_setup_entry(hass, config_entry) -> bool:
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][config_entry.entry_id] = coordinator
-
+    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
     hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
 
     return True
