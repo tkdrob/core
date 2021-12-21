1faa06c568a065371807e8770363190684bfe9ea
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-21 04:41:12 -0500

    Simplify hass data in airthings

--- homeassistant/components/airthings/__init__.py
+++ homeassistant/components/airthings/__init__.py
@@ -22,8 +22,6 @@ SCAN_INTERVAL = timedelta(minutes=6)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Airthings from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-
     airthings = Airthings(
         entry.data[CONF_ID],
         entry.data[CONF_SECRET],
@@ -46,8 +44,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     return True
