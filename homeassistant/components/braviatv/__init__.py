8c874cb602fec2379f24e938557d383157eaa550
Robert Hillis <tkdrob4390@yahoo.com>
2021-12-21 05:22:00 -0500

    Simplify hass data in braviatv

--- homeassistant/components/braviatv/__init__.py
+++ homeassistant/components/braviatv/__init__.py
@@ -36,9 +36,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][config_entry.entry_id] = coordinator
-
+    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
     hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
 
     return True
