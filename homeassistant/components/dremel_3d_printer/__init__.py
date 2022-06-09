aa95df00b3d7f5364867cb85710c463c372f01f1
Robert Hillis <tkdrob4390@yahoo.com>
2022-06-09 09:03:32 -0400

    uno mas

--- homeassistant/components/dremel_3d_printer/__init__.py
+++ homeassistant/components/dremel_3d_printer/__init__.py
@@ -7,12 +7,11 @@ from typing import Any
 from dremel3dpy import Dremel3DPrinter
 from requests.exceptions import ConnectTimeout, HTTPError
 
-from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.entity import DeviceInfo, Entity
-from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
@@ -23,32 +22,8 @@ from .const import _LOGGER, DOMAIN, PLATFORMS, SCAN_INTERVAL
 from .services import async_setup_services
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the OctoPrint component."""
-    if DOMAIN not in config:
-        return True
-
-    domain_config = config[DOMAIN]
-
-    for conf in domain_config:
-        hass.async_create_task(
-            hass.config_entries.flow.async_init(
-                DOMAIN,
-                context={"source": SOURCE_IMPORT},
-                data={
-                    CONF_HOST: conf[CONF_HOST],
-                },
-            )
-        )
-
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Dremel 3D Printer from a config entry."""
-    if DOMAIN not in hass.data:
-        hass.data[DOMAIN] = {}
-
     try:
         api = await hass.async_add_executor_job(
             Dremel3DPrinter, config_entry.data[CONF_HOST]
@@ -67,7 +42,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
         hass, f"{DOMAIN}-{config_entry.entry_id}", api, SCAN_INTERVAL.seconds
     )
     await coordinator.async_config_entry_first_refresh()
-    hass.data[DOMAIN][config_entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = coordinator
     hass.config_entries.async_setup_platforms(config_entry, PLATFORMS)
     await async_setup_services(hass)
     return True
@@ -75,11 +50,8 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Dremel config entry."""
-
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        del hass.data[DOMAIN]
-
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data.pop(DOMAIN)
     return unload_ok
 
 
@@ -145,11 +117,10 @@ class Dremel3DPrinterDeviceEntity(
     @property
     def device_info(self) -> DeviceInfo:
         """Return device information about this Dremel printer."""
-        api = self.coordinator.api
         return DeviceInfo(
             identifiers={(DOMAIN, self._entry_id)},
-            manufacturer=api.get_manufacturer(),
-            model=api.get_model(),
-            name=api.get_title(),
-            sw_version=api.get_firmware_version(),
+            manufacturer=self.coordinator.api.get_manufacturer(),
+            model=self.coordinator.api.get_model(),
+            name=self.coordinator.api.get_title(),
+            sw_version=self.coordinator.api.get_firmware_version(),
         )
