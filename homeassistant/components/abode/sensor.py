295b73d2a20a77fd40c1ac2c990528524655c1f8
Robert Hillis <tkdrob4390@yahoo.com>
2022-08-12 10:34:41 -0400

    Abode - Mixin value and unit functions

--- homeassistant/components/abode/sensor.py
+++ homeassistant/components/abode/sensor.py
@@ -1,6 +1,8 @@
 """Support for Abode Security System sensors."""
 from __future__ import annotations
 
+from collections.abc import Callable
+from dataclasses import dataclass
 from typing import cast
 
 from abodepy.devices.sensor import CONST, AbodeSensor as AbodeSense
@@ -17,21 +19,43 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from . import AbodeDevice, AbodeSystem
 from .const import DOMAIN
 
-SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
-    SensorEntityDescription(
+
+@dataclass
+class AbodeSensorEntityDescriptionMixin:
+    """Mixin for Abode sensor entities."""
+
+    unit_fn: Callable[[AbodeSense], str]
+    value_fn: Callable[[AbodeSense], float]
+
+
+@dataclass
+class AbodeSensorEntityDescription(
+    SensorEntityDescription, AbodeSensorEntityDescriptionMixin
+):
+    """Class describing abode sensor entities."""
+
+
+SENSOR_TYPES: tuple[AbodeSensorEntityDescription, ...] = (
+    AbodeSensorEntityDescription(
         key=CONST.TEMP_STATUS_KEY,
         name="Temperature",
         device_class=SensorDeviceClass.TEMPERATURE,
+        unit_fn=lambda device: cast(str, device.temp_unit),
+        value_fn=lambda device: cast(float, device.temp),
     ),
-    SensorEntityDescription(
+    AbodeSensorEntityDescription(
         key=CONST.HUMI_STATUS_KEY,
         name="Humidity",
         device_class=SensorDeviceClass.HUMIDITY,
+        unit_fn=lambda device: cast(str, device.humidity_unit),
+        value_fn=lambda device: cast(float, device.humidity),
     ),
-    SensorEntityDescription(
+    AbodeSensorEntityDescription(
         key=CONST.LUX_STATUS_KEY,
         name="Lux",
         device_class=SensorDeviceClass.ILLUMINANCE,
+        unit_fn=lambda device: cast(str, device.lux_unit),
+        value_fn=lambda device: cast(float, device.lux),
     ),
 )
 
@@ -54,32 +78,22 @@ class AbodeSensor(AbodeDevice, SensorEntity):
     """A sensor implementation for Abode devices."""
 
     _device: AbodeSense
+    entity_description: AbodeSensorEntityDescription
 
     def __init__(
         self,
         data: AbodeSystem,
         device: AbodeSense,
-        description: SensorEntityDescription,
+        description: AbodeSensorEntityDescription,
     ) -> None:
         """Initialize a sensor for an Abode device."""
         super().__init__(data, device)
         self.entity_description = description
         self._attr_name = f"{device.name} {description.name}"
         self._attr_unique_id = f"{device.device_uuid}-{description.key}"
-        if description.key == CONST.TEMP_STATUS_KEY:
-            self._attr_native_unit_of_measurement = device.temp_unit
-        elif description.key == CONST.HUMI_STATUS_KEY:
-            self._attr_native_unit_of_measurement = device.humidity_unit
-        elif description.key == CONST.LUX_STATUS_KEY:
-            self._attr_native_unit_of_measurement = device.lux_unit
+        self._attr_native_unit_of_measurement = description.unit_fn(device)
 
     @property
     def native_value(self) -> float | None:
         """Return the state of the sensor."""
-        if self.entity_description.key == CONST.TEMP_STATUS_KEY:
-            return cast(float, self._device.temp)
-        if self.entity_description.key == CONST.HUMI_STATUS_KEY:
-            return cast(float, self._device.humidity)
-        if self.entity_description.key == CONST.LUX_STATUS_KEY:
-            return cast(float, self._device.lux)
-        return None
+        return self.entity_description.value_fn(self._device)
